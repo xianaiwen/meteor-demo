ac3b42f32c322b2bcaf7dede8f1276f2874506f0
xianaiwen <xianaiwen@hotmail.com>
2017-05-23 14:51:05 +0800

    add project

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,14 @@
+// Template.postItem.helpers({
+//   domain: () => {
+//     let a = document.createElement('a')
+//     a.href = this.url
+//     return a.hostname
+//   }
+// })
+Template.postItem.helpers({
+  domain: function() {
+    var a = document.createElement('a');
+    a.href = this.url;
+    return a.hostname;
+  }
+});
